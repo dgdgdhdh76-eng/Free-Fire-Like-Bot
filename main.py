06ad4290ee1e6eb88e310c6cf919be661d2f4d15
dgdgdhdh76-eng <dgdgdhdh76@gmail.com>
2026-06-05 18:38:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def is_user_in_channel(user_id):
 
 
 def call_api(region, uid):
-    url = f"https://your-free-fire-like-api-domain/like?uid={uid}&server_name={region}"
+    url = f"https://your-free-fire-like-api-domain/like?uid={15568787504}&server_name={eu}"
     try:
         response = requests.get(url, timeout=20)
         if response.status_code != 200:
